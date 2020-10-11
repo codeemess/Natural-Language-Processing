1aac55002a4270745550a88defc2389df2b8a2fc
Pratik Deshpande <aristopratt.work@gmail.com>
2020-10-12 02:34:26 +0530

    Writing model

--- CS6320-UTD/Homeworks/HW3/Naive-Bayes-POS Tagging/NaiveBayes-POS.py
+++ CS6320-UTD/Homeworks/HW3/Naive-Bayes-POS Tagging/NaiveBayes-POS.py
@@ -6,9 +6,6 @@ Created on Mon Oct 12 01:04:35 2020
 @author: konoha
 """
 class POS_Tagging(object):
-    tags = {}
-    word_given_tag = {}
-    tag_given_tag = {}
     
     def buildTagsAndWordTags(self):
         f1 = open("POS-Tagged-Corpus.txt","r")
@@ -103,7 +100,21 @@ class POS_Tagging(object):
             split_content.append(z)
         
         return self.__bigramCounts(split_content)
+    
+    def test(self,sentence):
+        tags, word_given_tag, total_tags, tags_given_tags = self.buildTagsAndWordTags()
+        split_sentence = sentence.split()
+        print(split_sentence)
+        tags_given_words = {}
+        f1 = open("result.txt","w")
+        for i in range(0, len(split_sentence)):
+            if i==0:
+                pass
+            if i==len(split_sentence):
+                pass
+            
+
         
     
 p = POS_Tagging()
-p.TagsGivenTags()
+p.test("Hi I am Batman")
